b4a936b7e920ece97d75e67e808d449693a7dd3f
MaksHladun <makshladun@gmail.com>
2024-09-18 20:40:55 +0300

    Add About component and corresponding styles for the about page

---
+++ minting-dapp/src/components/About.tsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+export default function About() {
+  return (
+    <div className="about-page">
+      <Header />
+      <h1>About</h1>
+      <p>This is a simple minting dapp that allows users to mint NFTs.</p>
+    </div>
+  );
+}
