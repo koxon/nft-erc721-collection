94bed6111096731fef4e0b678cc1e17b531ba641
MaksHladun <makshladun@gmail.com>
2024-09-18 20:40:48 +0300

    Add ScrollToTopRouter component and update routes for improved navigation

--- minting-dapp/src/routes/index.tsx
+++ minting-dapp/src/routes/index.tsx
@@ -2,15 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { routePaths } from "./routePaths";
 import Home from "components/Home";
 import Mint from "components/Mint";
+import ScrollToTopRouter from "components/ScrollToTopRouter";
+import About from "components/About";
 
 const Router = () => (
   <>
     <BrowserRouter>
+      <ScrollToTopRouter />
       <Routes>
         <Route>
           <Route path={routePaths.home} element={<Home />} />
           <Route path={routePaths.mint} element={<Mint />} />
-          {/*<Route path={routePaths.about} element={<RaffleProfilePage />} /> */}
+          <Route path={routePaths.about} element={<About />} />
         </Route>
       </Routes>
     </BrowserRouter>
