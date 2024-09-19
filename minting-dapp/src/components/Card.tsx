bed5d4697dc1e201a55eeb752fa5a096eeac80fc
MaksHladun <makshladun@gmail.com>
2024-09-19 17:15:16 +0300

    refactor Card component with connection handling and improve responsive styles

--- minting-dapp/src/components/Card.tsx
+++ minting-dapp/src/components/Card.tsx
@@ -3,6 +3,8 @@ import nftPl from "../assets/img/fryhead-pl.png";
 import { ReactComponent as Eth } from "../assets/svg/eth-icon.svg";
 import MainButton from "./MainButton";
 import Loader from "./Loader";
+import { useAccount } from "wagmi";
+import { useConnectModal } from "@rainbow-me/rainbowkit";
 
 type CardProps = {
   nft: { name: string; description: string; backround: string };
@@ -12,6 +14,8 @@ type CardProps = {
 };
 
 export default function Card({ nft, tokenPrice, isLoading, onClick }: CardProps) {
+  const { isConnected } = useAccount();
+  const { openConnectModal } = useConnectModal();
   return (
     <div className="nft-card-box">
       <div className="img-box" style={{ background: nft.backround }}>
@@ -27,7 +31,7 @@ export default function Card({ nft, tokenPrice, isLoading, onClick }: CardProps)
         <p>{nft.description}</p>
         <div className="price-section">
           <span>Bind {isLoading ? <Loader width="14" /> : utils.formatEther(tokenPrice)} ETH</span>
-          <MainButton className={"secoundary-button"} title="Mint" onClick={onClick} />
+          <MainButton className={"secoundary-button"} title={isConnected ? "Mint" : "Connect"} onClick={isConnected ? onClick : openConnectModal} />
         </div>
       </div>
     </div>
