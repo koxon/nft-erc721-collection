cb7dc13f9b23776e83df5b60b558f85a2992c610
MaksHladun <makshladun@gmail.com>
2024-09-19 11:55:39 +0300

    Refactor Home component layout and styles for improved structure and responsiveness

--- minting-dapp/src/components/Home.tsx
+++ minting-dapp/src/components/Home.tsx
@@ -109,89 +109,91 @@ export default function Home() {
         <img src={mainImg} alt="" className="main-img" />
         <img src={mainBg} alt="" className="main-bg-img" />
       </div>
-      <div className="main-text">
-        <h1 className="main-title">Mint a NFT</h1>
-        <p className="sub-title">Give to charity</p>
-        <p className="title-text">
-          50% of the mint price and secondary sales fees <br />
-          <span>
-            are sent to
-            <Link to="https://thegivingblock.com/" target="_blank" rel="noreferrer">
-              <img src={givingblock} alt="" className="givingblock" />{" "}
-            </Link>
-            in perpetuity.
-          </span>
-        </p>
-        <MainButton
-          title={isConnected ? "Mint NFT" : "Connect wallet"}
-          onClick={!isConnected ? openConnectModal : () => navigate(generatePath(routePaths.mint))}
-        />
-      </div>
-
-      <div className="collection-section">
-        <div className="collection-header">
-          <div className="collection-title">
-            <h2>Notre Collection</h2>
-            <div>{isLoading ? <Loader width="16" /> : remainingNfts} Fry Heads to mint remaining</div>
-          </div>
-          <Link to="https://opensea.io/collection/fryheadsnft" target="_blank" rel="noreferrer">
-            See entire collection <Opensee />
-          </Link>
+      <div className="content-box">
+        <div className="main-text">
+          <h1 className="main-title">Mint a NFT</h1>
+          <p className="sub-title">Give to charity</p>
+          <p className="title-text">
+            50% of the mint price and secondary sales fees <br />
+            <span>
+              are sent to
+              <Link to="https://thegivingblock.com/" target="_blank" rel="noreferrer">
+                <img src={givingblock} alt="" className="givingblock" />{" "}
+              </Link>
+              in perpetuity.
+            </span>
+          </p>
+          <MainButton
+            title={isConnected ? "Mint NFT" : "Connect wallet"}
+            onClick={!isConnected ? openConnectModal : () => navigate(generatePath(routePaths.mint))}
+          />
         </div>
-
-        <div className="nft-cards">
-          {nfts.map((nft) => (
-            <Card
-              key={nft.name}
-              nft={nft}
-              tokenPrice={tokenPrice}
-              isLoading={isLoading}
-              onClick={!isConnected ? openConnectModal : () => navigate(generatePath(routePaths.mint))}
-            />
-          ))}
-        </div>
-      </div>
-      <Charities />
-      <div className="charity-text-section">
-        <div className="charity-text">
-          “Charity is the heartbeat of humanity, <br /> a testament to our shared <br /> compassion and commitment to <br /> uplifting one another.”
-        </div>
-        <MainButton
-          className={"secoundary-button"}
-          title={isConnected ? "Mint NFT" : "Connect wallet"}
-          onClick={!isConnected ? openConnectModal : () => navigate(generatePath(routePaths.mint))}
-        />
-      </div>
-      <div className="contact-section">
-        <div className="mail" onClick={sendEmail}>
-          Contact@fryheads.com
-        </div>
-        <div className="contract">
-          Contract address
-          <Link
-            to={
-              isTestnet
-                ? `https://sepolia.etherscan.io/address/${CollectionConfig.contractAddressTest}`
-                : `https://etherscan.io/address/${CollectionConfig.contractAddress}`
-            }
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={etherscan} alt="" />
-          </Link>
-        </div>
-        <div className="find-us">
-          Find us on
-          <span>
-            <Link to="https://twitter.com/FryHeadsNFT" target="_blank" rel="noreferrer">
-              <X />
-            </Link>
+        <div className="collection-section">
+          <div className="collection-header">
+            <div className="collection-title">
+              <h2>Notre Collection</h2>
+              <div>{isLoading ? <Loader width="16" /> : remainingNfts} Fry Heads to mint remaining</div>
+            </div>
             <Link to="https://opensea.io/collection/fryheadsnft" target="_blank" rel="noreferrer">
-              <OpenseeBl />
+              See entire collection <Opensee />
             </Link>
-          </span>
+          </div>
+
+          <div className="nft-cards">
+            {nfts.map((nft) => (
+              <Card
+                key={nft.name}
+                nft={nft}
+                tokenPrice={tokenPrice}
+                isLoading={isLoading}
+                onClick={!isConnected ? openConnectModal : () => navigate(generatePath(routePaths.mint))}
+              />
+            ))}
+          </div>
+        </div>
+        <Charities />
+        <div className="charity-text-section">
+          <div className="charity-text">
+            “Charity is the heartbeat of humanity, <br /> a testament to our shared <br /> compassion and commitment to <br /> uplifting one another.”
+          </div>
+          <MainButton
+            className={"secoundary-button"}
+            title={isConnected ? "Mint NFT" : "Connect wallet"}
+            onClick={!isConnected ? openConnectModal : () => navigate(generatePath(routePaths.mint))}
+          />
+        </div>
+        <div className="contact-section">
+          <div className="mail" onClick={sendEmail}>
+            Contact@fryheads.com
+          </div>
+          <div className="contract">
+            Contract address
+            <Link
+              to={
+                isTestnet
+                  ? `https://sepolia.etherscan.io/address/${CollectionConfig.contractAddressTest}`
+                  : `https://etherscan.io/address/${CollectionConfig.contractAddress}`
+              }
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={etherscan} alt="" />
+            </Link>
+          </div>
+          <div className="find-us">
+            Find us on
+            <span>
+              <Link to="https://twitter.com/FryHeadsNFT" target="_blank" rel="noreferrer">
+                <X />
+              </Link>
+              <Link to="https://opensea.io/collection/fryheadsnft" target="_blank" rel="noreferrer">
+                <OpenseeBl />
+              </Link>
+            </span>
+          </div>
         </div>
       </div>
+
       <img src={bottomImg} alt="" className="bottom-img" />
       <Footer />
     </div>
